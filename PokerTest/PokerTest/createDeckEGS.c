190e4d6007d4eba6e729b09277dc792a0f981799
AuroraDawn <shaymothy_thromin_the_roman@yahoo.com>
2016-03-04 18:42:49 -0800

    a

--- PokerTest/PokerTest/createDeckEGS.c
+++ PokerTest/PokerTest/createDeckEGS.c
@@ -5,6 +5,7 @@ Creates a deck of 52 cards (unshuffled).
 Author: Emily Sandness
 History:
 02 Mar 2016 - Initial Version.
+03 Mar 2016 - Fixed create bug.
 */
 
 #include "pokerHeader.h"
@@ -22,7 +23,7 @@ void createDeckEGS(CARD deck[]) {
   for (i = 0; i < CARDS_IN_DECK; i++) {
 
     //create the face of the card (1-13)
-    deck[i].cardFace = (i % CARDS_IN_SUIT);
+    deck[i].cardFace = (i % CARDS_IN_SUIT) + 1;
     //create the suit of the card (0-3)
     deck[i].cardSuit = i / CARDS_IN_SUIT;
   }
